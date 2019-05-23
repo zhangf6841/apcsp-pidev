9758183248c5dbd1291243a6db0d969f76177908
Faith <zhangf6841@my.sduhsd.net>
2019-05-23 12:04:39 -0700

    iteration

---
+++ wloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  // standard for loop = from 0
+  for (int i = 0; i < 100; i++)
+    {
+    int div = 3;
+            if (i%div==0)
+               printf("%d\n", i);
+    }
+}
